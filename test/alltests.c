d90caa8cd18ae5563aeaffb1bccd74032c6936c2
CeKaGe <github@cekage.net>
2017-09-03 21:51:15 +0200

    Removes wboa tests. Not needed anymore.

--- test/alltests.c
+++ test/alltests.c
@@ -29,7 +29,7 @@
 #include "_wbac_compare.c"
 #include "_wbac_setstring.c"
 #include "_conffile.c"
-#include "_wboa_init.c"
+//#include "_wboa_init.c"
 #include "_jsonauth.c"
 #include "_jsonentries.c"
 #include "_performentries.c"
@@ -45,7 +45,7 @@ int main(int argc, char** argv) {
     RUN_SUITE(_wbac_compare);
     RUN_SUITE(_wbac_setstring);
     RUN_SUITE(_conffile);
-    RUN_SUITE(_wboa_init);
+//    RUN_SUITE(_wboa_init);
     RUN_SUITE(_jsonauth);
     RUN_SUITE(_jsonentries);
     RUN_SUITE(_performentries);
